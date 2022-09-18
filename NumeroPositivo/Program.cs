3442d31264cb13dd60d7bfe835de792adad2e404
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-09-18 00:33:15 -0300

    Initial commit

---
+++ NumeroPositivo/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+* Programa C# para aceitar um número do usuário e exibi-lo se for positivo
+*/
+
+using System;
+
+namespace NumeroPositivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                Console.WriteLine("O número é positivo");
+            }
+            else if (numero == 0)
+            {
+                Console.WriteLine("O número é 0");
+            }
+            else
+            {
+                Console.WriteLine("O número é negativo");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
